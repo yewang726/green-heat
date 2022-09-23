6dc42c4d44b8a5053c72968b4baa5a210cb0a1cc
ahmadmojiri <98721000+ahmadmojiri@users.noreply.github.com>
2022-09-23 12:21:15 +1000

    Added a simple GreenH2-test for testing the server

---
+++ GUI/Dash_codes/GreenH2-test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ahmad Mojiri
+"""
+
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+
+
+colors = {'background': 'whitesmoke',
+          'text': 'black'}
+
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+            dbc.Row(dbc.Col(html.H1('Green H2'),
+                            width={'size':6, 'offset':5}
+                      )),
+            
+            dbc.Row(dbc.Col(html.H2('Solar Thermal Group - Australian National University'),
+                            width={'size':7, 'offset':3}
+                      )),
+            ])
+            
+            
+            
+          
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
