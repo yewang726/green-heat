01df8cdf12c40fdce1d8cb3dd9f1b0585c57be8b
ahmadmojiri <98721000+ahmadmojiri@users.noreply.github.com>
2022-10-08 20:48:57 +1100

    Update projdirs.py

--- GUI/Dash_codes/projdirs.py
+++ GUI/Dash_codes/projdirs.py
@@ -18,7 +18,7 @@ else:
 
 datadir = basedir + "DATA%s" %connector
 # modeldir = basedir + "modelling%spython%spackage%s" %(connector, connector, connector)
-optdir = basedir + "minizinc%s" %(connector)
+optdir = basedir + "MINIZINC%s" %(connector)
 # figdir = basedir + "modelling%sfigures%s" %(connector, connector)
 # paperdir = basedir + "Publications%spaper_1%s" %(connector, connector)
 # resultsdir = datadir + "arbitrage%s" %connector
