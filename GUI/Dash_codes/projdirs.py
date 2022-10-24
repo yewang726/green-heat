ae999cf7648fc8522a50b27095791199439d1650
ahmadmojiri <98721000+ahmadmojiri@users.noreply.github.com>
2022-10-24 18:32:02 +1100

    debugging

--- GUI/Dash_codes/projdirs.py
+++ GUI/Dash_codes/projdirs.py
@@ -6,13 +6,15 @@ Created on Tue Mar 29 10:32:04 2022
 """
 
 import os,platform
+
  
 connector = ['\\','/'][platform.system()=='Linux']
 
 if platform.system()=='Linux':
     basedir = r'/home/ahmadmojiri/GreenH2/'
 else:
-    basedir = os.path.realpath('..') + connector
+    # basedir = os.path.realpath('..') + connector
+    basedir = r'C:\\Nextcloud\\HILT-CRC---Green-Hydrogen\\'
     
     
 
